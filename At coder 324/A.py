16dd566d781fc772db2ede4705c874a387fcfefd
Manobal Singh Bagady <manobalsinghbagady.bt21ele@pec.edu.in>
2023-10-14 19:59:04 +0530

    at coder round 324

---
+++ At coder 324/A.py
@@ -0,0 +1,27 @@
+# ---------------- MSB's Coding Template ---------------- #
+"""
+    "ɴᴏ ᴄᴏᴅᴇ ɪꜱ ᴘᴇʀꜰᴇᴄᴛ"
+"""
+
+# ---I/O from file---#
+import sys
+
+try:
+    sys.stdin = open("input.txt", "r", encoding="UTF-8")
+    sys.stdout = open("output.txt", "w", encoding="UTF-8")
+    sys.stderr = open("output.txt", "w", encoding="UTF-8")
+except FileNotFoundError as __:
+    pass
+
+
+# ---------------------- Code Starts Here ----------------------#
+
+n = int(input())
+l = list(map(int, input().split()))
+
+res = "Yes"
+for i in range(1, n):
+    if l[i] != l[i - 1]:
+        res = "No"
+
+print(res)
